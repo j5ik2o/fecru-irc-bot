4c63aaf389f7dcc3173678230376f8f68609e88f
Junichi Kato <junichi_kato@dwango.co.jp>
2012-04-27 03:56:23 +0900

    add new files.

---
+++ src/main/resources/admin.js
@@ -0,0 +1,55 @@
+AJS.toInit(function() {
+    var baseUrl = AJS.$("meta[name='application-base-url']").attr("content");
+    
+    function populateForm() {
+        AJS.$.ajax({
+            url: baseUrl + "/rest/fecru-irc-bot/1.0/globalConfig",
+            dataType: "json",
+            success: function(config) {
+            	if (config.enable){
+                	AJS.$("#enable").attr('checked', 'checked');
+                }else{
+                	AJS.$("#enable").removeAttr("checked");
+                }
+                AJS.$("#ircServerName").attr("value", config.ircServerName);
+                AJS.$("#ircServerPort").attr("value", config.ircServerPort);
+            }
+        });
+    }
+    
+    function updateConfig() {
+        AJS.$.ajax({
+            url: baseUrl + "/rest/fecru-irc-bot/1.0/globalConfig",
+            type: "PUT",
+            contentType: "application/json",
+            data: '{ "enable": "' + AJS.$("#enable").attr("checked") + '", "ircServerName": "' + AJS.$("#ircServerName").attr("value") + '", "ircServerPort": ' +  AJS.$("#ircServerPort").attr("value") + ' }',
+            processData: false
+        });
+    }
+
+    function submitCheck(){
+        var check = AJS.$("#enable").attr('checked') && AJS.$("#ircServerName").val != "" && AJS.$("#ircServerPort").val != "";
+        //alert(check);
+        if (check){
+            AJS.$("#submit").removeAttr("disabled");
+        }else{
+            AJS.$("#submit").attr("disabled", "disabled");
+        }
+    }
+
+    populateForm();
+    submitCheck();
+    pageModified = false;
+    
+    AJS.$("#admin").submit(function(e) {
+        e.preventDefault();
+        updateConfig();
+    });
+    AJS.$("#ircServerName").blur(function(){
+        submitCheck();
+    });
+    AJS.$("#ircServerPort").blur(function(){
+        submitCheck();
+    });
+
+});
---
+++ src/main/resources/project.js
@@ -0,0 +1,62 @@
+AJS.toInit(function() {
+    var baseUrl = AJS.$("meta[name='application-base-url']").attr("content");
+    var projectId = AJS.$("#projectId").attr("value");
+    function populateForm() {
+    	AJS.$.ajax({
+            url: baseUrl + "/rest/fecru-irc-bot/1.0/globalConfig",
+            dataType: "json",
+            success: function(config) {
+            	if (config.enable){
+                	AJS.$("#project").removeAttr("disabled");
+                	AJS.$("#enable").removeAttr("disabled");
+                	AJS.$("#notice").removeAttr("disabled");
+                	AJS.$("#channelName").removeAttr("disabled");
+                	AJS.$("#submit").removeAttr("disabled");	
+                }else{
+                	AJS.$("#project").attr("disabled", "disabled");
+                	AJS.$("#enable").attr("disabled", "disabled");
+                	AJS.$("#notice").attr("disabled", "disabled");
+                	AJS.$("#channelName").attr("disabled", "disabled");
+                	AJS.$("#submit").attr("disabled", "disabled");
+                }
+            }
+        });
+        AJS.$.ajax({
+            url: baseUrl + "/rest/fecru-irc-bot/1.0/channelConfig/" + projectId,
+            dataType: "json",
+            success: function(config) {   
+                if (config.enable){
+                	AJS.$("#enable").attr('checked', 'checked');
+                }else{
+                	AJS.$("#enable").removeAttr("checked");
+                }
+                if (config.notice){
+                	AJS.$("#notice").attr('checked', 'checked');
+                }else{
+                	AJS.$("#notice").removeAttr("checked");
+                }
+                AJS.$("#channelName").attr("value", config.channelName);
+            }
+        });
+    }
+    
+    function updateConfig() {
+        AJS.$.ajax({
+            url: baseUrl + "/rest/fecru-irc-bot/1.0/channelConfig/" + projectId,
+            type: "PUT",
+            contentType: "application/json",
+            data: '{ "enable": "' + AJS.$("#enable").attr("checked") +
+                '", "notice": "' + AJS.$("#notice").attr("checked") +
+                '", "channelName": "' +  AJS.$("#channelName").attr("value") + '" }',
+            processData: false
+        });
+    }
+    
+    populateForm();
+    pageModified = false;
+    
+    AJS.$("#project").submit(function(e) {
+        e.preventDefault();
+        updateConfig();
+    });
+});
